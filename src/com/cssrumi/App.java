8100600ac4845a9be6c49ca17f98b36a4dd23ed3
Adam Rumiński <Adam.Ruminski@atena.pl>
2018-11-09 15:58:29 +0100

    empty project

--- src/com/cssrumi/App.java
+++ src/com/cssrumi/App.java
@@ -1,16 +1,9 @@
 package com.cssrumi;
 
+
 public class App {
 
     public static void main(String[] args) {
-        // write your code here
-        int value = 7;
-//        value = value / 0;
-        String[] texts = {"one", "two", "three"};
-        try {
-            System.out.println(texts[3]);
-        } catch (RuntimeException e) {
-            System.out.println(e.toString());
-        }
+
     }
 }
