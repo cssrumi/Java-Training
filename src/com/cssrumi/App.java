ad32fc8083ce3f5666cb743676f14acea432920e
Adam Rumiński <Adam.Ruminski@atena.pl>
2018-11-14 15:06:51 +0100

    c

--- src/com/cssrumi/App.java
+++ src/com/cssrumi/App.java
@@ -1,9 +1,22 @@
 package com.cssrumi;
 
 
+import javax.swing.*;
+
 public class App {
 
     public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame("Hello World");
+
+                frame.setSize(600, 500);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
 
     }
 }
