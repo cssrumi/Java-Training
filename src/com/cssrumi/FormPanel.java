97e6d44b98d4ece21e06c236668de8995f393ec1
Adam Rumiński <Adam.Ruminski@atena.pl>
2018-11-14 17:52:13 +0100

    12

--- src/com/cssrumi/FormPanel.java
+++ src/com/cssrumi/FormPanel.java
@@ -28,7 +28,54 @@ public class FormPanel extends JPanel {
         Border outerBorder = BorderFactory.createEmptyBorder(5,5,5,5);
         setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
 
+        setLayout(new GridBagLayout());
 
+        GridBagConstraints gc = new GridBagConstraints();
+
+        Insets insetsNull = new Insets(0,0,0,0);
+        Insets insetsRight5 = new Insets(0,0,0,5);
+
+        // FIRST ROW
+        gc.weightx = 1;
+        gc.weighty = 0.1;
+
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.fill = GridBagConstraints.NONE;
+        gc.anchor = GridBagConstraints.LINE_END;
+        gc.insets = insetsRight5;
+        add(nameLabel, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 0;
+        gc.anchor = GridBagConstraints.LINE_START;
+        gc.insets = insetsNull;
+        add(nameField, gc);
+
+        // SECOND ROW
+        gc.weightx = 1;
+        gc.weighty = 0.1;
+
+        gc.gridy = 1;
+        gc.gridx = 0;
+        gc.insets = insetsRight5;
+        gc.anchor = GridBagConstraints.LINE_END;
+        add(occupationLabel, gc);
+
+        gc.gridy = 1;
+        gc.gridx = 1;
+        gc.anchor = GridBagConstraints.LINE_START;
+        gc.insets = insetsNull;
+        add(occupationField, gc);
+
+        // THIRD ROW
+        gc.weightx = 1;
+        gc.weighty = 2.0;
+        gc.gridy = 2;
+        gc.gridx = 1;
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
+        gc.insets = insetsNull;
+        add(okBtn, gc);
     }
 
 }
